888e6cb03f94cad47709a9076a05ef176f78d49b
Anderson Nunes <dande.nunes.94@gmail.com>
2023-01-23 16:51:11 -0300

    funções data tempo #1 módulo PHP intermediário

---
+++ dataTempo.php
@@ -0,0 +1,14 @@
+<!-- documentação parra formatação de data tempo -->
+<!-- https://www.php.net/manual/pt_BR/function.date -->
+
+<?php
+// formato de data
+$dataAtual = date("d/m/Y  \á\s H:i:s");//há dferença entre letra maiúsculas e minusculas;
+
+
+echo $dataAtual;
+
+
+
+
+?>
