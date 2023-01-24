fda31f0f2a20fabbaf298e7cef752b50e3bdbe53
Anderson Nunes <dande.nunes.94@gmail.com>
2023-01-24 01:52:49 -0300

    sessão & cookie php intermediário

---
+++ sessao&coockie.php
@@ -0,0 +1,16 @@
+<?php
+//armazenar informações no servidor;
+//PHP cria cookies e sessões o JS apenas cookies;
+// session_start();//lemba de sempre colocar na primeira linha do PHP, pois se declarar algo antes, pode dar ero no PHP;
+//sessão
+// $_SESSION["teste"] = "Anderson Nunes";
+// echo "Sessão foi feita...";
+
+// echo "Meu nome é: ".$_SESSION["teste"];
+
+//cookie
+//setcookie("meusteste", "Fulano de tal", time()+3600);//1º parametro idetificação do cookie, 2º parametro o qe terá nele(o valor), 3º parametro tempo de vida;
+//echo "Coookie setado com sucesso";
+echo "Meu cookie é de: ".$_COOKIE["meusteste"];
+
+?>
--- sql.php
+++ sql.php
@@ -1,6 +1,6 @@
 <?php
 
-$autor = addslashes($_POST["autor"]);//'or 1=1' -> siulação de erro
+$autor = addslashes($_POST["autor"]);// addslashes() -> através de aplicação de uma '\' será barra qualquer inserção ilicítaa com a finalidade de prejudicar o sistema;     'or 1=1' -> siulação de erro
 
 $sql = "SELECT * FROM posts WHERE autor = '' or '1=1'";
 
