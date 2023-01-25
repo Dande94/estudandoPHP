cdc977747d7beed4e1a6d26f1245bec8adade52e
Anderson Nunes <dande.nunes.94@gmail.com>
2023-01-25 16:38:20 -0300

    CRUD simples com banco de dados - php intermediário

---
+++ crudSimples/conexaoBanco.php
@@ -0,0 +1,6 @@
+<?php
+$dsn = "mysql:dbname=blog3;host=127.0.0.1";
+$dbuser = "root";
+$dbpass = "";
+$pdo = new PDO($dsn, $dbuser, $dbpass);
+?>
---
+++ crudSimples/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    table{
+        width:70%;
+    }
+    th{
+        text-align:left;
+    }
+
+</style>
+<body>
+<table>
+    <thead>
+        <th>Usuário</th>
+        <th>Email</th>
+        <th>Ações</th>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table>
+</body>
+</html>
