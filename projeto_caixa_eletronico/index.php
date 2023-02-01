5890a19e38f4ea82b67fbf5d231e08e41890052f
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-01 00:20:30 -0300

    projeto caixa eletronico pt.4 - php intemediário

--- projeto_caixa_eletronico/index.php
+++ projeto_caixa_eletronico/index.php
@@ -3,6 +3,18 @@ session_start();
 require_once "conexaoBanco.php";
 
 if(isset($_SESSION['banco']) && !empty($_SESSION['banco'])){
+    $id = $_SESSION['banco'];
+    $sql = "SELECT * FROM contas WHERE id = :id";
+    $sql = $pdo->prepare($sql);
+    $sql->bindValue(':id', $id);
+    $sql->execute();
+
+    if($sql->rowCount()>0){
+        $info = $sql->fetch();
+    }else{
+        header("Location: login.php");
+        exit;
+    }
 
 }else{
     header("Location: login.php");
@@ -22,9 +34,10 @@ if(isset($_SESSION['banco']) && !empty($_SESSION['banco'])){
 </head>
 <body>
     <h1>Banco dos Dev's</h1>
-    <h3>Agência:000 </h3>
-    <h3>Conta:000 </h3>
-    <h4>Saldo:000 </h4>
+    <h2>Tiular: <?php echo $info['titular']?> </h2>
+    <h3>Agência: <?php echo $info['agencia']?> </h3>
+    <h3>Conta: <?php echo $info['conta']?> </h3>
+    <h4>Saldo: <?php echo $info['saldo']?> </h4>
 
     <a href="sair.php">Sair</a>
 
