e9a667c60ba42406445b7ab27fc618f00b193469
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-01 01:22:27 -0300

    projeto caixa eletronico pt.6 - php intemediário

--- projeto_caixa_eletronico/index.php
+++ projeto_caixa_eletronico/index.php
@@ -32,6 +32,20 @@ if(isset($_SESSION['banco']) && !empty($_SESSION['banco'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Caixa Eletrônico</title>
 </head>
+<style>
+    table{
+        width:40%;
+        border: 2px solid #999;
+    }
+    th{
+        text-align:left;
+        border-bottom:2px solid #999;
+    }
+     tr:nth-child(even){
+        background-color:#ddd;
+    }
+
+</style>
 <body>
     <h1>Banco dos Dev's</h1>
     <h2>Tiular: <?php echo $info['titular']?> </h2>
@@ -41,5 +55,39 @@ if(isset($_SESSION['banco']) && !empty($_SESSION['banco'])){
 
     <a href="sair.php">Sair</a>
     <hr>
+    <a href="add_transacao.php">Adicionar Transação</a>
+    <h3>Movimentações / Extrato</h3>
+    <table>
+    <thead>
+        <th>Data</th>
+        <th>Valor</th>
+    </thead>
+    <tbody>
+        <?php
+            $sql = "SELECT * FROM histórico WHERE id_conta = :id_conta";
+            $sql = $pdo->prepare($sql);
+            $sql->bindValue(':id_conta', $id);
+            $sql -> execute();
+            if($sql->rowCount() > 0){
+                foreach($sql->fetchAll() as $item){
+                    ?>
+                    <tr>
+                        <td><?php echo date('d/m/Y H:i', strtotime($item['data_operacao']));?></td>
+                        <td>
+                            <?php if($item['tipo'] == '0'):?>
+                                <span color="green">R$ <?php echo $item['valor'];?></span>
+                            <?php else:?>    
+                                <span color="tomato">R$ <?php echo $item['valor'];?></span>
+                            <?php endif;?>    
+                            
+                        </td>
+                    </tr>'
+                    <?php
+                }
+            }
+        ?>
+    </tbody>
+</table>
+
 </body>
 </html>
