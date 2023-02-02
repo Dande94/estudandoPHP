069b7d559285c22320d618d177f9e8f60e84c0fa
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-01 23:00:09 -0300

    projeto sistemas de tags pt.2 - php intemediário

--- projetoTags/index.php
+++ projetoTags/index.php
@@ -6,18 +6,43 @@ $sql = $pdo->prepare($sql);
 $sql->execute();
 if($sql->rowCount() > 0){
     $lista =$sql->fetchAll();
+
+    // echo "<pre>";
+    // print_r($lista);
+    // echo "</pre>";
+
+    $carac = array();
+
+    foreach($lista as $usuario){
+        $palavras = explode(",", $usuario['caracteristicas']);
+        foreach($palavras as $palavra){
+            $palavra = trim($palavra);
+            if(isset($carac[$palavra])){
+                $carac[$palavra]++;
+            }else{
+                $carac[$palavra] = 1;
+            }
+        }
+    }
+    // echo "<pre>";
+    // print_r($carac);
+    // echo "</pre>";
+
+    arsort($carac);
+
+    $palavras = array_keys($carac);
+    $contagens = array_values($carac);
+
+    $maior= max($contagens);
+    $tamanhos=array(11,15,20,30);
+
+    for($x=0;$x<count($palavras);$x++){
+        $n = $contagens[$x] / $maior;
+
+        $h = ceil($n * count($tamanhos));
+
+        echo"<p style='font-size:".$tamanhos[$h - 1]."px'>".$palavras[$x]."(".$contagens[$x].")"."</p>";
+    }
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
