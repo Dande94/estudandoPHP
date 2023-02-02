63003cf63bb19e3c66417673d7945a4780e6fcbe
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-02 04:22:03 -0300

    exercicio conversor pt.2 - php intemediário

--- conversordePalavraEmDigito/conversao.php
+++ conversordePalavraEmDigito/conversao.php
@@ -1,16 +1,43 @@
 <?php
+require "index.php";
 if(isset($_POST["palavras"]) && !empty($_POST["palavras"])){
     $palavras = $_POST["palavras"];
     $arrayPalavras =explode(",",$palavras);
-    print_r($arrayPalavras);
-    
-    $substitutas = array ("1","2","3","4","5","6","7","8","9");
-    
-    for($arrayPalavras as $palavra){
-      
-   
+    $textos = array();
+    $converter = function($num) {
+    switch ($num) {
+        case "um":
+        return "1";
+        case "dois":
+        return "2";
+        case "tres":
+        return "3";
+        case "quatro":
+        return "4";
+        case "cinco":
+        return "5";
+        break;
+        case "seis":
+        return "6";
+        break;
+        case "sete":
+        return "7";
+        break;
+        case "oito":
+        return "8";
+        break;
+        case "nove":
+        return "9";
+        break;
+        default:
+        return "não reconhecido";
+        break;
     }
-    
+    };
+    // print_r($textos);
+    $textos = array_map($converter, $arrayPalavras);
+    $convertido = implode(",",$textos);
+    echo  $convertido;
 }
 
 ?>
