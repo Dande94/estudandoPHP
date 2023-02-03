d5899afdea7f3b2c561e394a2d61e886d8b49f3c
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-03 16:29:24 -0300

    exercicio tabuada - php intemediário

---
+++ exercicioTabuada/index.php
@@ -0,0 +1,32 @@
+<style>
+body{
+    display: flex;
+}
+tr:nth-child(even){
+    background: #8AF6BA;
+}
+tr:nth-child(odd){
+    background: #F59289;
+}
+td{
+    padding:0.5rem;
+    text-align:center;
+}
+
+</style>
+<body>
+    <table border='1'>
+    <?php
+    for($i = 1; $i <=10; $i++){
+        echo "<tr>";
+        for($j = 1; $j <=10; $j++){
+            echo "<td>";
+            echo $i * $j;
+            echo "</td>";
+        }
+        echo "</tr>";
+        echo "<br>";
+    }
+    ?>
+    </table>
+</body>
