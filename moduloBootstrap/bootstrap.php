547cdd3fd37f5d06187aae78891b6d005bb95bea
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-06 20:06:00 -0300

    aprendendo Bootstrap pt.19 - modulo Bootstrap dentro do curso de PHP

--- moduloBootstrap/bootstrap.php
+++ moduloBootstrap/bootstrap.php
@@ -505,7 +505,38 @@
     <button class="btn btn-primary" type="submit">Submit form</button>
   </div>
 </form>
+
 <hr>
+
+<h5>Modal ou Light-Box</h5>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img class="img-fluid" src="4038512.jpg"  alt="">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
 <hr>
 <hr>
 <hr>
