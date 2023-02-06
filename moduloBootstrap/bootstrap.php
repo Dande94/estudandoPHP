d985f6fc9a2afe3b2b1b3724a8390d4f13fa2d70
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-05 21:05:08 -0300

    aprendendo Bootstrap pt.10- modulo Bootstrap dentro do curso de PHP

--- moduloBootstrap/bootstrap.php
+++ moduloBootstrap/bootstrap.php
@@ -284,6 +284,17 @@
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 <hr>
+<h5>badges</h5>
+<button type="button" class="btn btn-primary">
+  Notifications <span class="badge text-bg-secondary">4</span>
+</button>
+<button type="button" class="btn btn-primary position-relative">
+  Inbox
+  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    99+
+    <span class="visually-hidden">unread messages</span>
+  </span>
+</button>
 <hr>
 <hr>
 <hr>
