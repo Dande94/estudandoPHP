aa41848a52ed1bada332813d46edd4ba96a04eaa
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-07 20:42:12 -0300

    aprendendo Bootstrap pt.17 - modulo Bootstrap dentro do curso de PHP

---
+++ moduloBootstrap/bootstrap4.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.2.3-dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="container">
+        <h5>grid + resposividade</h5>
+        <div class="row ">
+            <div class="col" style="background-color:#F00">...</div>
+            <div class="col" style="background-color:#0F0">...</div>
+            <div class="col" style="background-color:#F00">...</div>
+        </div>
+        <br>
+        <div class="row ">
+            <div class="col" style="background-color:#F00">...</div>
+            <div class="col" style="background-color:#0F0">...</div>
+        </div>
+        <br>
+        <div class="row ">
+            <div class="col-sm" style="background-color:#F00">...</div>
+            <div class="col-sm" style="background-color:#0F0">...</div>
+            <div class="col-sm" style="background-color:#F00">...</div>
+        </div>
+        <br>
+        <div class="row ">
+            <div class="col-md" style="background-color:#F00">...</div>
+            <div class="col-md" style="background-color:#0F0">...</div>
+            <div class="col-md" style="background-color:#F00">...</div>
+        </div>
+        <br>
+        <div class="row ">
+            <div class="col-lg" style="background-color:#F00">...</div>
+            <div class="col-lg" style="background-color:#0F0">...</div>
+            <div class="col-lg" style="background-color:#F00">...</div>
+        </div>
+        <br>
+        <div class="row ">
+            <div class="col-sm" style="background-color:#F00">...</div>
+            <div class="col" style="background-color:#0F0">...</div>
+            <div class="col" style="background-color:#F00">...</div>
+        </div>
+        <br>
+    </section>
+    <hr>
+    <script src="bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="bootstrap-5.2.3-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
