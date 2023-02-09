0d70e011d2f6dcabe38937a226cf3d895189ce11
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-09 10:44:03 -0300

    aprendendo Bootstrap pt.31 - modulo Bootstrap dentro do curso de PHP

--- moduloBootstrap/bootstrap4.php
+++ moduloBootstrap/bootstrap4.php
@@ -587,7 +587,36 @@
                     <a class="dropdown-item" href="#">Forgot password?</a>
                 </div>
             </div>
-   
+        </section>
+        <hr>
+        <section class="container">
+            <h5>form</h5>
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="exemplo@exemplo.com">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            <br>
+            <form class="row">
+                <div class="col mb-3">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email:">
+                </div>
+                <div class="col mb-3">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha:">
+                </div>
+                <button type="submit" class="col btn btn-primary mb-3">Submit</button>
+            </form>
         </section>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
