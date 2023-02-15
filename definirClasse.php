6b0b98c8f56a01b9089b555fcc57f51cb5a593ff
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-15 18:32:34 -0300

    Classes pt.2 - Modulo PHP Avançado

--- definirClasse.php
+++ definirClasse.php
@@ -1,6 +1,23 @@
 <?php
 //como definir uma classe
+
+// ►Modificadores de acesso(em metodos e atributos;) -
+// ►►Public - pode ser acessado por qualquer classe do sistema;
+// ►►►Protected - pode ser acessado desde que ele seja herdado, trasmitido por herança;
+// ►►►►Package(default) - só pode ser acessado por classes do mesmo pacote;
+// ►►►►►Private - o mais restrito, só pode ser acessado de dentro da própria classe;
+
 class Pessoa{
-    
+
+    public function trocarSenha(){
+        //conectarBanco();
+        //verificações
+        //trocar a senha
+
+    }
+
+    private function conectarBanco(){
+
+    }
 }
 ?>
