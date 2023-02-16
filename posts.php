4cb60a9e55984a1ea2a01cff381e5595782a15e1
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-16 18:58:14 -0300

    Construtor  - Modulo PHP Avançado

--- posts.php
+++ posts.php
@@ -6,20 +6,38 @@ class Post{
     private $corpo;
     private $comentarios;
 
+    public function __construct($t,$c){//construtor, raramente não está publico;
+        //chamando as funções e usando os parametro para construir o corpo da postagem;
+        $this->setTitulo($t);
+        $this->setCorpo($c);
+    }
+    
+    //getters - está "pegando" o valor armazenado nas variaveís, para poder manipular;
     public function getTitulo(){
         return $this->titulo;
     }
+    public function getCorpo(){
+        return $this->corpo;
+    }
+    //setters -. nesse caso o setter tá sendo usado para armazenar um valor nas variáveis 
     public function setTitulo($t){
         if(strlen($t) > 5 && is_string($t)){
             $this->titulo = $t;
         }
     }
+    public function setCorpo($c){
+        if(strlen($c) > 5 && is_string($c)){
+            $this->corpo = $c;
+        }
+    }
 }
 
+//nesse caso a manipulação se dá através da exibição do qu está armazenado;
+$post = new Post("Titulo Qualquer","Corpo da minha postagem");
+echo"Titulo: ".$post->getTitulo();
+echo "<br>";
+echo"Postagem: ".$post->getCorpo();
 
-$post = new Post();
-//$post->setTitulo(array());//não passa na valdação
-// $post->setTitulo("Titulo da postagem");//alterar 
-// echo"Titulo: ".$post->getTitulo();//pegar o valor armazenado
+
 
 ?>
