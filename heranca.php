460e40e8483bbbf92a14ca66f1bc357035de33c3
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-16 20:50:27 -0300

    Herança(abstração)  - Modulo PHP Avançado

--- heranca.php
+++ heranca.php
@@ -1,29 +1,48 @@
 <?php
 //exemplo com RPG
-class Status{
+abstract class Status{
     public $nome;
     private $idade;
     private $especie;
     private $defesa;
     private $saude;
+
+    abstract protected function atacar();//ao usar uma função abstrata ela deve sempre ter protected, a classe recebe também uma abstract, e isso obriga todos os herdeiros a ter essa função;
+
+    public function setNome($n){
+        $this->nome = $n;
+    }
+    public function getNome(){
+        return $this->nome;
+    }
+
 }
 class Guerreiro extends Status{
     private $montaria;
     private $arma;
+    public function atacar(){
+        return true;//aqui usei return qualquer apenas remover a expressão de erro;
+    }
 }
 class Mago extends Status{
     private $magias;
     private $livros;
+    public function atacar(){
+        return true;
+    }
 }
 class Elfo extends Status{
     private $arma;
     private $carga;
     private $magias;
+    public function atacar(){
+        return true;
+    }
 }
 
 $guerreiro =  new Guerreiro();
-$guerreiro->nome="Alastor";
-echo "O nome do guerreiro é: ".$guerreiro->nome;
+$guerreiro->setNome("Alastor");
+echo "O nome do guerreiro é: ".$guerreiro->getNome();
 
 ?>
  
