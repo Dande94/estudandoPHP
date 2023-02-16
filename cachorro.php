a3bcbfd988162af437f091d665d1e517a2e9d71d
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-15 21:34:44 -0300

    Classes pt.4 - Modulo PHP Avançado

--- cachorro.php
+++ cachorro.php
@@ -3,14 +3,24 @@ class Cachorro{
     public function latir(){
         echo " au au";
     }
+
+    public $nome;
+    private $idade;
+
 }
 
-$rex = new Cachorro();//instanciando a classe 
-$rex->latir();// acessando a função dentro da classe;
+// $rex = new Cachorro();//instanciando a classe 
+// $rex->latir();// acessando a função dentro da classe;
 
-$fido = new Cachorro(); 
-$fido->latir();
+//--outro cachorro usando as mesma carteristicas;
+// $fido = new Cachorro(); 
+// $fido->latir();
 
-// Cachorro::latir();//executando a classe de modo indefinido, possível apenas para acessos publicos, objeto não manuseavél; para funcionar aplicar o static; não aconselhavél
+// Cachorro::latir();//executando a classe de modo indefinido, possível apenas para acessos publicos, objeto não manuseavél; para funcionar aplicar o static; não aconselhavél;
+
+$cachorro = new Cachorro();
+$cachorro->nome="Rex"; // definindo o nome;
+
+echo"O nome do cão é ".$cachorro->nome;
 
 ?>
