21674d805534c51495f53ef809a71b5c5bf2dddd
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-17 22:39:56 -0300

    Polimorfismo  - Modulo PHP Avançado

---
+++ polimorfismo.php
@@ -0,0 +1,31 @@
+<?php
+//no php o polimorfismo uma função é sobreescrita pra mais próxima da classe que herda algo da classe pai;
+//mo exemplo abaixo ele usou o mesmo nome pra as funções que competem a preferencia de execução;
+
+class Animal{
+    public function getNome(){
+        echo "getNome da classe Animal";
+    }
+
+    public function testar(){
+        echo "testagem";
+    }
+}
+
+class Cachorro extends Animal{
+
+    // public function getNome(){
+    //     echo "getNome da classe Cachorro";
+    // }
+
+    public function getNome(){
+        $this->testar();
+    }
+}
+
+$cachorro = new Cachorro();
+$cachorro->getNome();
+
+
+//segundo bonieky polimorfimo é quando subtituo uma função herdada pela função próprio objeto;
+?>
