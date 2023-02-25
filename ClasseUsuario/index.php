44cf236b31b34971a39ccf2199d67cc248fabd34
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-25 04:56:14 -0300

    Classe Usuario pt.6 - Modulo PHP Avançado

--- ClasseUsuario/index.php
+++ ClasseUsuario/index.php
@@ -1,3 +1,18 @@
 <?php
 require_once "usuario.php";
+//adicionar
+// $usuario = new Usuario();
+// $usuario->setEmail("exemplo2@exemplo.com");
+// $usuario->setSenha("123");
+// $usuario->setNome("Beltranissímo de Tasso");
+// $usuario->salvar();
+// echo "<script>alert('Usuário salvo com sucesso!')</script>";
+//-----
+//pegar as informações;
+$usuario = new Usuario(13);
+// echo "O nome do usuario é: ".$usuario->getNome();
+//atualizar
+$usuario->setNome("Fulanissimo de Tal");
+$usuario->salvar();
+echo "<script>alert('Usuário atualizado com sucesso!')</script>";
 ?>
