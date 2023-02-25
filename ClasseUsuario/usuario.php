b01f4aee2235f488f112fd6069a915dc08914117
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-25 04:16:41 -0300

    Classe Usuario pt.2 - Modulo PHP Avançado

--- ClasseUsuario/usuario.php
+++ ClasseUsuario/usuario.php
@@ -4,5 +4,34 @@ class Usuario{
     private $email;
     private $senha;
     private $nome;
+
+    //id:
+    public function getID(){
+        return $this->id;
+    }
+    
+    //email:
+    public function setEmail($e){
+        $this->email = $e;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+    
+    //senha:
+    public function setSenha($s){
+        $this->senha = $s;
+    }
+    
+    //nome:
+    public function setNome($n){
+        $this->nome = $n;
+    }
+    public function getNome(){
+        return $this->nome;
+    }
+
+
+
 }
 ?>
