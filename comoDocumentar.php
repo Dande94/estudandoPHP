9d0488a9e21bd0fa8c1160f1d4b203be5be8bf3d
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-26 07:41:18 -0300

    Como Doumentar - Modulo PHP Avançado

---
+++ comoDocumentar.php
@@ -0,0 +1,21 @@
+<?php
+/*
+*
+* Função media();
+*
+* Está função somar os valores $x e $y e dividir por $z para extrair a média;
+*
+* @package ControleDeEstoque //de qual projeto essa função faz parte
+* @author Anderson Nunes <exemplo@exemplo.com> //quem é o autor dessa função e como entrar em contato 
+*
+* @param $x float é o primeiro valor a ser somado;
+* @param $y float é o segundo valor a ser somado;
+* @param $z int é o valor que servirá de divisor;
+* @return float;
+*/
+
+function media($x,$y,$z){
+    return ($x + $y)/$z;
+}
+
+?>
