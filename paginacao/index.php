05816d6c6732f8392d64f337f6f62cd20a965407
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-26 07:28:40 -0300

    Paginação pt.2 - Modulo PHP Avançado

--- paginacao/index.php
+++ paginacao/index.php
@@ -29,20 +29,28 @@ try{
 //     $sql->execute();  
 // }
 
+$qt_por_pagina = 10;
+
 //traz a quantidade de post do banco de dados
 $total=0;
 $sql="SELECT COUNT(*) as c FROM posts";
 $sql= $pdo->query($sql);
 $sql=$sql->fetch();
 $total=$sql['c'];
-$paginas = $total / 10;
+$paginas = $total / $qt_por_pagina;
 //echo $total;
 //exit;//ao aplicar o exit no meio do código ele irá parar o código naquela linha;
 
 
 //percorre o banco de dados
-$p=0;
-$sql = "SELECT * FROM posts LIMIT $p,10";
+$pg = 1;
+if(isset($_GET['p']) && !empty($_GET['p'])){//se o 'p' lá na url existir e estiver setado:
+    $pg=addslashes($_GET['p']);//o '$pg' recebe o 'p';
+}
+
+$p=($pg-1) * $qt_por_pagina;//lógica para limite de item por página;
+
+$sql = "SELECT * FROM posts LIMIT $p,$qt_por_pagina";
 $sql = $pdo->query($sql);
 if($sql->rowCount() > 0){
     foreach($sql->fetchAll() as $item){
@@ -50,6 +58,10 @@ if($sql->rowCount() > 0){
     }
 }
 
+echo "<hr>";
+for($q=0;$q<$paginas;$q++){//loop de paginação
+    echo "<a href='./?p=".($q+1)."'>[".($q+1)."]</a>";//a syntax './?' volta para mesma página
+}
 
 
 ?>
