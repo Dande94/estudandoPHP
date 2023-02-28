2ba50d3e7392e9d2176e308ca577e80abd457bea
Anderson Nunes <dande.nunes.94@gmail.com>
2023-02-28 15:11:54 -0300

    Cadastro com aprovação pt.3  - Modulo PHP Avançado

--- cadastroComAprocacao/index.php
+++ cadastroComAprocacao/index.php
@@ -3,7 +3,21 @@ if(isset($_POST['nome']) && !empty($_POST['nome'])){
     $nome=addslashes($_POST['nome']);
     $email=addslashes($_POST['email']);
     require_once "conexao.php";
-    
+    $pdo->query("INSERT INTO usuarios SET nome='$nome', email='$email'");
+    $id = $pdo->lastInsertId();//A função lastInsertId() retorna o ID do último registro inserido em uma tabela do banco de dados usando a conexão PDO (PHP Data Object) em PHP. Essa função é geralmente usada após a execução de uma instrução INSERT em uma tabela que possui uma coluna de autoincremento
+
+    //criar um hash para servir como código de liberação;nese caso irá se basear n id do usuário;
+    $md5=md5($id);
+    $link= 'http://www.b7web.com.br/cadastroconfirma/confirma.php?h='.$md5;
+
+    //normalmente se criar uma variavel para armazenar o email de envio, porém como o mesmo que é recebido será o que será enviado; então será reutilizado a variavel de recebimento;
+    //estrutura que irá compor o a função mail() do php;
+    $assunto="Confirme seu Cadastro";
+    $msg="Clique no link abaixo para confirmar seu cadastro:\n\n".$link;
+    $headers="From: anderson.nunes@exemplo.com"."\r\n"."X-Mailer: PHP/".phpversion();
+
+    //função mail();
+    mail($email, $assunto, $msg, $headers);//A função mail() do PHP é usada para enviar e-mails. Ela aceita vários parâmetros,1ºparam o endereço de e-mail do destinatário,2ºparam o assunto do e-mail,3ºparam o corpo da mensagem e 4ºparam cabeçalhos opcionais.
 }
 
 ?>
