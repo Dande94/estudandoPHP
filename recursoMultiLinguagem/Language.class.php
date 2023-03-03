2ff61aea135c2c29fc38aa751ddd65288016e027
Anderson Nunes <dande.nunes.94@gmail.com>
2023-03-03 15:29:28 -0300

    Recurso Multilinguagem pt.2 - Modulo PHP Avançado

---
+++ recursoMultiLinguagem/Language.class.php
@@ -0,0 +1,13 @@
+<?php
+//gerenciador de linguagem
+class Language{
+    private $l; //variavel para armazenar o idioma padrão e posteiormente armazenar outros idiomas;
+    public function __construct(){
+            $this->l = "pt-br";//setando um idioma padrão
+    }
+    public function get(){
+
+    }
+}
+
+?>
--- recursoMultiLinguagem/index.php
+++ recursoMultiLinguagem/index.php
@@ -1,1 +1,15 @@
+<?php
+session_start();// o idioma será salvo na sessão;
+require "Language.class.php";
+ $lang = new Language();
+
+ //verificar o envio do lang
+    if(!empty($_GET['lang']));
+?>
+<!-- botões para indexar o idioma -->
+<a href="index.php?lang=en">English</a>
+<a href="index.php?lang=pt-br">Português</a>
+<a href="index.php?lang=es">Español</a>
+<hr>
+
  <button><?php echo $lang->get('BUY') ?></button>
