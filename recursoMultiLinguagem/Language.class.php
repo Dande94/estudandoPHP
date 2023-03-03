c9a0746ed724d321e872422a057d4add1703d1c5
Anderson Nunes <dande.nunes.94@gmail.com>
2023-03-03 15:54:40 -0300

    Recurso Multilinguagem pt.3 - Modulo PHP Avançado

--- recursoMultiLinguagem/Language.class.php
+++ recursoMultiLinguagem/Language.class.php
@@ -2,10 +2,25 @@
 //gerenciador de linguagem
 class Language{
     private $l; //variavel para armazenar o idioma padrão e posteiormente armazenar outros idiomas;
+    private $ini; //será uma array que ira armazenar o conteudo de arquivos ini;
     public function __construct(){
             $this->l = "pt-br";//setando um idioma padrão
+            //verificar a existencia de uma linguagem já setada
+            if(!empty($_SESSION['lang']) && file_exists('lang/'.$_SESSION['lang'].'.ini')){//caso retorne verdadeiro é pq tem uma linguagem setada aqui;
+                //agregando ao if a verificação de existência do arquivo que contém o dicionário file_exists(), caso o usuário tente enviar qualquuer coisa via url para a session;
+                //--
+                $this->l = $_SESSION['lang'];//aplicando o idioma a variavel $l que foi setada na session;
+
+                //trazer o dicionário;
+                //O método parse_ini_file é utilizado no PHP para analisar arquivos INI (arquivos de configuração de sistema) e converter suas informações em um array associativo. É muito comum utilizar arquivos INI para armazenar configurações do sistema, como por exemplo informações de conexão com o banco de dados, caminhos de arquivos, parâmetros de aplicação, entre outros.
+                // O parse_ini_file analisa o arquivo INI especificado no parâmetro filename e retorna um array associativo que contém as seções e chaves do arquivo. O segundo parâmetro opcional, process_sections, indica se as seções devem ser processadas como subarrays. Se o valor de process_sections for true, cada seção será um subarray do array retornado, caso contrário todas as seções e chaves serão retornadas no mesmo array.
+                $this->ini = parse_ini_file('lang/'.$this->l.'.ini');//através do this irá direcionar o dicionário, o parse_ini_file, converterá em array, e tudo será armazenado na variavel $ini;
+            }
     }
-    public function get(){
+    public function getLanguage(){
+        return $this->l;
+    }
+    public function get($word, $return = false){
 
     }
 }
--- recursoMultiLinguagem/index.php
+++ recursoMultiLinguagem/index.php
@@ -1,15 +1,21 @@
 <?php
 session_start();// o idioma será salvo na sessão;
+
+//verificar o envio do lang
+if(!empty($_GET['lang'])){
+    $_SESSION['lang'] = $_GET['lang'];//capturando a lang passada via url, e setando na session;
+}
+
 require "Language.class.php";
  $lang = new Language();
 
- //verificar o envio do lang
-    if(!empty($_GET['lang']));
 ?>
 <!-- botões para indexar o idioma -->
 <a href="index.php?lang=en">English</a>
 <a href="index.php?lang=pt-br">Português</a>
 <a href="index.php?lang=es">Español</a>
 <hr>
+Linguagem Definida:<?php echo $lang->getLanguage(); ?> 
+<hr>
 
- <button><?php echo $lang->get('BUY') ?></button>
+ <button><?php echo $lang->get('BUY');?></button>
