8f7ef449874f5f22c0eb1a0e70eeada7144df8e9
Anderson Nunes <dande.nunes.94@gmail.com>
2023-07-03 19:56:44 -0300

    Modulo 12 - projeto permissao_acesso pt.3

--- projeto_permissao_acesso/classes/usuarios.class.php
+++ projeto_permissao_acesso/classes/usuarios.class.php
@@ -1,6 +1,9 @@
 <?php
 class Usuario{
     private $pdo;
+    private $id;
+    private $permissoes;
+
     public function __construct($pdo)
     {
         $this->pdo =$pdo;
@@ -14,7 +17,22 @@ class Usuario{
 
         if($sql->rowCount() > 0){
             $sql = $sql->fetch();
-            $_SESSION['logado'] = $sql['id'];
+            $_SESSION['logado'] = $sql['id'];//criada a sessão, depois que validou-se que há a existencia da conta do usuário;
+            return true;
+        }
+            return false;//caso não encontre o registro, retornará false;
+    }
+    public function setUsuario($id){
+        $this->id = $id;
+        $sql = "SELECT * FROM usuarios WHERE id = :id";
+        $sql = $this->pdo->prepare($sql);
+        $sql->bindValue(":id",$id);
+        $sql->execute();
+
+        if($sql->rowCount() > 0){
+            $sql= $sql->fetch();
+
+            $this->permissoes = explode(",",$sql['permissoes']);//captura os dados da coluna permissao  do bando de dados e transforma em um array com a explode; e salva na variavel permissoes;
         }
     }
 }
--- projeto_permissao_acesso/index.php
+++ projeto_permissao_acesso/index.php
@@ -2,10 +2,14 @@
 session_start();
 
 require_once "conexao.php";
+require_once "classes/usuarios.class.php";
 
 if(!isset($_SESSION['logado'])){//verifica se o usuario está logado para ter acesso a página index
     header("Location: login.php");//se não tiver envia ele pra página de login;
     exit;//pra encerrar a execução do restante do código;
 }
 
+$usuarios = new Usuario($pdo);
+$usuarios->setUsuario($_SESSION['logado']);
 ?>
+<h3>Sistema</h3>
