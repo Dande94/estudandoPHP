3c473dcbccc9674d4ccf69c67a7f659d81d6356c
Anderson Nunes <dande.nunes.94@gmail.com>
2023-07-17 16:58:25 -0300

    Modulo 12 - Projeto Usuarios Online pt.2

--- projetoQdtUsuariosOnline/index.php
+++ projetoQdtUsuariosOnline/index.php
@@ -5,20 +5,27 @@ date_default_timezone_set('America/Sao_Paulo');
 $ip = $_SERVER['REMOTE_ADDR'];
 $hora = date('H:i:s');
 
+//registrar o acesso do IP
 $sql = "INSERT INTO acessos (ip,hora) VALUES (:ip,:hora)";
 $sql = $pdo->prepare($sql);
 $sql->bindValue(":ip",$ip);
 $sql->bindValue(":hora",$hora);
 $sql->execute();
 
+//deletar registro com mais de 5 minutos
+$sql = "DELETE FROM acessos WHERE hora < :hora";
+$sql=$pdo->prepare($sql);
+$sql->bindValue(":hora",date('H:i:s', strtotime('-5 minutes')));
+$sql->execute();
+
+//contar os registro dentro dos 5 minutos
 $sql = "SELECT * FROM acessos WHERE hora > :hora GROUP BY ip";
 $sql = $pdo->prepare($sql);
 $sql->bindValue(":hora",date('H:i:s', strtotime('-5 minutes')));
 $sql->execute();
 $contagem = $sql->rowCount();
-echo $contagem;
+echo "User's Online: ".$contagem;
 
-// $sql = "DELETE FROM acessos ";
 
 
 
