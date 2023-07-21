342847bd89aaedb6f657d75031b1020edf6a1083
Anderson Nunes <dande.nunes.94@gmail.com>
2023-07-21 15:54:10 -0300

    Saindo um pouco do Tutorial Hell pt.1

--- exercicioCalcularDiferencaDeTempo/index.php
+++ exercicioCalcularDiferencaDeTempo/index.php
@@ -1,9 +1,9 @@
 <?php
-$tempoEspera = 2;
+$tempoEspera = 5;
 header("refresh: $tempoEspera");
 //______________________________
 require_once 'tempo.class.php';
-$t = '2015-01-13 08:00:00';
+$t = '2023-07-19 21:00:00';
 $data = date('d/m/Y H:i:s', strtotime($t));
 echo $data.'<br/> foi há '.Tempo::diferenca($t).' atrás';
 
