08e342983f252968d9503b87d20eb60c4f56dc57
Anderson Nunes <dande.nunes.94@gmail.com>
2023-07-25 17:32:37 -0300

    Modulo 15 - Projeto Sistema de Classificados pt.4

--- projetoSiteDeClassificados/index.php
+++ projetoSiteDeClassificados/index.php
@@ -1,39 +1,16 @@
-<!doctype html>
-<html lang="pt-br">
+<?php require_once 'pages/header.php';?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Classificados</title>
-    <!-- styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- styles -->
-</head>
-
-<body>
-    
-    <nav class="navbar bg-dark navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand text-light" href="#">Classificados</a>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav text-light">
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="#">Cadastre-se</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light">Login</a>
-                    </li>
-                </ul>
-            </div>
+<div class="container card text-bg-light my-3">
+    <div class="card-body text-align-center">
+            <h1 class="card-title">Nós temos hoje XXX anúncios</h1>
+            <p class="card-text">E mais de XXX usuários cadastrados!</p>
         </div>
-    </nav>
-    </nav>
-    <!-- scripts -->
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <script src="assets/js/script.js"></script>
-    <!-- scripts -->
-</body>
-
-</html>
+    </div>
+    <div class="row mx-2">
+        <div class="col-sm-4">
+            <h5>Busca avançada</h5>
+        </div>
+        <div class="col-sm-8"><h5>Últimos Anúncios</h5></div>
+    </div>
+    
+<?php require_once 'pages/footer.php';?>
---
+++ projetoSiteDeClassificados/pages/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Classificados</title>
+    <!-- styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- styles -->
+</head>
+<body>
+    <nav class="navbar bg-dark navbar-expand-lg rounded-bottom">
+        <div class="container-fluid">
+            <a class="navbar-brand text-light" href="#">Classificados</a>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav text-light">
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Cadastre-se</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
