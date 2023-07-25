fa814eb475494f1a62767f9caa7aefdbae112bed
Anderson Nunes <dande.nunes.94@gmail.com>
2023-07-25 17:09:17 -0300

    Modulo 15 - Projeto Sistema de Classificados pt.3

--- projetoSiteDeClassificados/index.php
+++ projetoSiteDeClassificados/index.php
@@ -1,20 +1,39 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Classificados</title>
     <!-- styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
     <!-- styles -->
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+</head>
+
+<body>
+    
+    <nav class="navbar bg-dark navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand text-light" href="#">Classificados</a>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav text-light">
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Cadastre-se</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    </nav>
     <!-- scripts -->
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script src="assets/js/script.js"></script>
     <!-- scripts -->
-  </body>
+</body>
+
 </html>
