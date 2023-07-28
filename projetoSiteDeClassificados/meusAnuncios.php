e982233039d8a55ddb7b53ca087acbb08be31b15
Anderson Nunes <dande.nunes.94@gmail.com>
2023-07-28 17:07:44 -0300

    Modulo 15 - Projeto Sistema Classificados pt.11

---
+++ projetoSiteDeClassificados/meusAnuncios.php
@@ -0,0 +1,20 @@
+<?php require_once 'pages/header.php'; ?>
+<main class="container">
+    <h2>Meus Anúncios</h2> 
+
+    <table class="table table-striped">
+        <thead>
+            <th>Foto</th>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Ações</th>
+        </thead>
+        <tbody>
+            <?php
+            ?>
+        </tbody>
+    </table>
+</main>
+
+
+<?php require_once 'pages/header.php'; ?>
