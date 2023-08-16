3d6e2b25940c86c900f81dd6d611df26dfe95913
Anderson Nunes <dande.nunes.94@gmail.com>
2023-08-16 18:51:38 -0300

    Modulo 4(Curso Atualizado) - Exercicio listando arquivos em txt pt.2

--- CursoPHPAtualizado/Modulo4/exercicioComTXT/index.php
+++ CursoPHPAtualizado/Modulo4/exercicioComTXT/index.php
@@ -1,18 +1,40 @@
 <?php 
 $nome = filter_input(INPUT_POST,'nome',FILTER_SANITIZE_SPECIAL_CHARS);
-if(!empty($_POST["nome"]) && $nome){
+
+function listagemDeNomes(){
+    $pegarNomes = file_get_contents('nome.txt');
+    $listarNomes = explode("\n", $pegarNomes);
+    return $listarNomes;
+}
+function verificarRepeticao($nome){
+    $lista = listagemDeNomes();
+    foreach($lista as $item){
+        // if($item == $nome){
+        //     return false;
+        // }
+        if(strcasecmp(trim($item), trim($nome)) === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+if($nome && !empty($_POST["nome"]) ){
     if(file_exists('nome.txt')){
-        $listaDeNome = file_get_contents('nome.txt');
-        $listaDeNome .= "{$nome} \n";
-        file_put_contents('nome.txt',$listaDeNome);
+        $validation = verificarRepeticao($nome);
+        if($validation){
+            $listaDeNome = file_get_contents('nome.txt');
+            $listaDeNome .= "{$nome} \n";
+            file_put_contents('nome.txt',$listaDeNome);
+        }
     }else{
-        file_put_contents('nome.txt',$nome."\n");    
+        file_put_contents('nome.txt',$nome."\n");
     }
 }
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,19 +42,19 @@ if(!empty($_POST["nome"]) && $nome){
 </head>
 <body>
     <form action="" method="post">
-        <label for="">Novo nome:
+        <label for="">Novo nomes:
             <br>
             <input type="text" name="nome" id="">
             <input type="submit" value="Adicionar">
         </label>
     </form>
 <hr>
+<h3>Lista dos Nome</h3>
     <ul>
 <?php 
     if(file_exists('nome.txt')){
-        $pegarNomes = file_get_contents('nome.txt');
-        $listarNomes = explode("\n", $pegarNomes);
-        foreach($listarNomes as $nome){
+        $nomes = listagemDeNomes();
+        foreach($nomes as $nome){
             if(!empty($nome)){
                 echo "<li>{$nome}</li>";
             }
