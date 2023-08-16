d8975c94531f0d0aa70040d3def94175af2bd3da
Anderson Nunes <dande.nunes.94@gmail.com>
2023-08-16 19:31:30 -0300

    Modulo 4(Curso Atualizado) - Movendo | Renomeando | Copiando arquivos

---
+++ CursoPHPAtualizado/Modulo4/movendoRenomeandoCopiandoArquivos/index.php
@@ -0,0 +1,8 @@
+<?php 
+//renomar arquivos
+//rename("teste.txt","teste.txt2");//1º caminho até o arquivo, 2º novo nome;
+//mover arquivos
+//rename("teste.txt2", "pastaTeste/teste.txt2");//1º caminho até o arquivo, 2º onde ele irá ficar;
+//copiar arquivos
+copy("pastaTeste/teste.txt2", "teste3.txt");//1º caminho até o arquivo que será copiado, 2º onde ele será colado;
+?>
