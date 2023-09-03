785857da4e47eaeec29c6ad014086255a810ca30
Anderson Nunes <dande.nunes.94@gmail.com>
2023-09-02 23:50:19 -0300

    Modulo 6(Curso Atualizado) - composer pt.1

---
+++ CursoPHPAtualizado/Modulo6/composer/classes/matematica/Basica.php
@@ -0,0 +1,9 @@
+<?php 
+namespace matematica;// ao elaborar o namespace aso tenha mais diretórios, inverter a barra -> matematica\algo\exemplo;
+
+class Basica{
+    public function somar($x,$y){
+        return $x + $y;
+    }
+}
+?>
---
+++ CursoPHPAtualizado/Modulo6/composer/index.php
@@ -0,0 +1,8 @@
+<?php 
+require_once "vendor/autoload.php";
+
+$m = new matematica\Basica();
+echo $m->somar(10,15);
+
+
+?>
