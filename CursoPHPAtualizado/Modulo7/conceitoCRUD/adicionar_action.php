0971cf782de2d873d4ace5d3a92bbf708cccc42a
Anderson Nunes <dande.nunes.94@gmail.com>
2023-09-03 16:25:30 -0300

    Modulo 7(Curso Atualizado) - CRUD: CREATE pt.2

--- CursoPHPAtualizado/Modulo7/conceitoCRUD/adicionar_action.php
+++ CursoPHPAtualizado/Modulo7/conceitoCRUD/adicionar_action.php
@@ -5,13 +5,25 @@ $name_exemplo = filter_input(INPUT_POST,'name_exemplo',FILTER_SANITIZE_SPECIAL_C
 $email_exemplo = filter_input(INPUT_POST,'email_exemplo', FILTER_VALIDATE_EMAIL);
 
 if($name_exemplo && $email_exemplo){
-    $sql = "INSERT INTO usuarios (nome,email) VALUES (:nome,:email)";
+
+     
+    $sql = "SELECT * FROM usuarios WHERE email = :email";
     $sql = $pdo->prepare($sql);
-    $sql->bindValue(":nome", $name_exemplo);
     $sql->bindValue(":email", $email_exemplo);
     $sql->execute();
 
-    header("Location: index.php");
+    if($sql->rowCount() == 0){
+            $sql = "INSERT INTO usuarios (nome,email) VALUES (:nome,:email)";
+            $sql = $pdo->prepare($sql);
+            $sql->bindValue(":nome", $name_exemplo);
+            $sql->bindValue(":email", $email_exemplo);
+            $sql->execute();
+        
+            header("Location: index.php");
+    }else{
+        header("Location: adicionar.php");
+        exit;
+    }
 
 }else{
     header("Location: adicionar.php");
